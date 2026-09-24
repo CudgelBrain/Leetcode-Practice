7f0ccb084750dea37cfe6468c076c4224f48b796
Sneha <sneh.bsspr@gmail.com>
2026-09-24 17:58:55 +0900

    reverse array

---
+++ reverseArray.js
@@ -0,0 +1,11 @@
+const arr =  [1,2,3,4,5];
+
+const reverseArray = function(arr){
+  let reversed = [];
+  for(let i=(arr.length)- 1 ;i>=0;i--){
+    reversed.push(arr[i]);
+  }
+  return reversed;
+}
+
+console.log(reverseArray(arr))
